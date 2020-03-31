4479a8511cc82ff913527d55a194e7d2e79a4bd9
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2020-03-31 16:27:39 -0300

    Exercício 4.15 Serviço de Distância aponta para MongoDB

--- fj33-eats-distancia-service/src/main/java/br/com/caelum/eats/distancia/Restaurante.java
+++ fj33-eats-distancia-service/src/main/java/br/com/caelum/eats/distancia/Restaurante.java
@@ -1,32 +1,27 @@
 package br.com.caelum.eats.distancia;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
+@Document(collection = "restaurantes")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Restaurante {
 
-	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Id
 	private Long id;
 
 	@NotBlank @Size(max=9)
 	private String cep;
 
-	private Boolean aprovado;
-
-	@Column(nullable=false)
 	private Long tipoDeCozinhaId;
 
 }
