c902bc108fc79e0f61609bd1cfc7644999baabed
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2020-04-02 15:30:59 -0300

    Exercício 9.9, 9.11 e 9.14 Simula demora no serviço de Distância, Circuit Breaker no RestTemplate com @HystrixCommand e Fallback com @HystrixCommand

--- api-gateway/src/main/java/br/com/caelum/apigateway/DistanciaRestClient.java
+++ api-gateway/src/main/java/br/com/caelum/apigateway/DistanciaRestClient.java
@@ -1,10 +1,13 @@
 package br.com.caelum.apigateway;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @Service
 class DistanciaRestClient {
@@ -18,11 +21,16 @@ class DistanciaRestClient {
 		this.distanciaServiceUrl = distanciaServiceUrl;
 	}
 
+	@HystrixCommand(fallbackMethod = "restauranteSemDistancia")
 	@SuppressWarnings("unchecked")
 	Map<String, Object> porCepEId(String cep, Long restauranteId) {
 		String url = distanciaServiceUrl + "/restaurantes/"+cep+"/restaurante/"+restauranteId;
 		return restTemplate.getForObject(url, Map.class);
 	}
 	
+	
+	Map<String, Object> restauranteSemDistancia(String cep, Long restauranteId) {
+		return new HashMap<>();
+	}
 	
 }
