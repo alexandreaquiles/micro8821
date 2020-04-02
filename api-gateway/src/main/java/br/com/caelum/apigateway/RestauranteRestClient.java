18858b70775c14c54b3bf033b8e6124892360096
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2020-04-02 15:58:24 -0300

    Exercícios 9.16, 9.18 e 9.20 Simula demora no Monólito, Circuit Breaker no Feign com Hystrix e Fallback no Feign

--- api-gateway/src/main/java/br/com/caelum/apigateway/RestauranteRestClient.java
+++ api-gateway/src/main/java/br/com/caelum/apigateway/RestauranteRestClient.java
@@ -1,15 +1,31 @@
 package br.com.caelum.apigateway;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="monolito")
+@FeignClient(name="monolito", fallback = RestauranteRestClientFallback.class)
 interface RestauranteRestClient {
 
 	@GetMapping("/restaurantes/{restauranteId}")
 	Map<String, Object> porId(@PathVariable("restauranteId") Long restauranteId);
+	
+}
+
+@Component
+class RestauranteRestClientFallback implements RestauranteRestClient {
+
+	@Override
+	public Map<String, Object> porId(Long restauranteId) {
+		//TODO: busca do cache
+		HashMap<String, Object> resultado = new HashMap<>();
+		resultado.put("restauranteId", restauranteId);
+		resultado.put("outraInformacao", "valorDaOutraInformacao");
+		return resultado;
+	}
 	
 }
