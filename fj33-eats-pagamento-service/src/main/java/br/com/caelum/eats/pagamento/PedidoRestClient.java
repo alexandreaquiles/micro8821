d2e8d58fffeca6c66e428a01a62d14e92211aa0d
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2020-04-02 11:10:23 -0300

    Exercícios 7.11 e 7.14 Client-side Balancing no Feign com Ribbon

--- fj33-eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/PedidoRestClient.java
+++ fj33-eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/PedidoRestClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
-@FeignClient(name="pedido", url="${configuracao.pedido.service.url}")
+@FeignClient(name="monolito")
 public interface PedidoRestClient {
 
 	@PutMapping("/pedidos/{pedidoId}/pago")
